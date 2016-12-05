8388ad1ad882cac9416ec12cf394083613557fef
Mathieu Simard <mathieu.simard.14@gmail.com>
2016-12-05 01:49:57 -0500

    Script modif

--- js/infinitescroll.js
+++ js/infinitescroll.js
@@ -42,63 +42,69 @@ function loadMoreContent() {
 
 /* ********* WINDOW LOAD ********** */
 $(window).load(function() {
-    masonry_gallery(jQuery)
-    beforeafter(jQuery)
+    masonry_gallery()
     var paginationnav = $("div.paginationnav").first();
     paginationnav.hide();
 });	
 	
-function masonry_gallery($)
+function masonry_gallery()
 {
   
+  (function( $ ){
     jQuery(window).load(function() {
-	var $masonry_gallery = jQuery("div.gallery.masonry-gallery.post-list").first();
-	if ( $masonry_gallery.length > 0 ) {
-		$masonry_gallery.each( function(index, element) {
-			var $masonry_items = $(element).find('.gallery-item');
-		
-			// set masonry layout
-			$(element).isotope({
-				masonry: { columnWidth: $(element).find('.gallery-item')[0] },
-				itemSelector: '.gallery-item'
-			});
-			$(element).isotope('layout');
-				
-			// filtering
-			jQuery('#gallery-filter li a').on('click', function(){
-				jQuery('#gallery-filter li a').removeClass('active');
-				jQuery(this).addClass('active');
-				var selector = jQuery(this).attr('data-filter');
-				$masonry_gallery.isotope({ filter: selector });
-				return false;
-			});
+	    // masonry gallery
+	    var $masonry_gallery = jQuery('.masonry-gallery.gallery');
+	    if ( $masonry_gallery.length > 0 ) {
 
-			// changing layout
-			jQuery('#grid-changer li a').on('click', function(){
-				jQuery('#grid-changer li a').removeClass('active');
-				jQuery(this).toggleClass('active');
+		    $masonry_gallery.each( function(index, element) {
+			    var $masonry_items = $(element).find('.gallery-item');
+		    
+			    // set masonry layout
+			    $(element).isotope({
+				    masonry: { columnWidth: $(element).find('.gallery-item')[0] },
+				    itemSelector: '.gallery-item'
+			    });
+			    $(element).isotope('layout');
+				    
+			    // filtering
+			    jQuery('#gallery-filter li a').on('click', function(){
+				    jQuery('#gallery-filter li a').removeClass('active');
+				    jQuery(this).addClass('active');
+				    var selector = jQuery(this).attr('data-filter');
+				    $masonry_gallery.isotope({ filter: selector });
+				    return false;
+			    });
 
-				$masonry_items.removeClass('col-3');
-				$masonry_items.removeClass('col-4');
-				$masonry_items.removeClass('col-5');
-				$masonry_items.toggleClass(jQuery(this).closest('li').attr('class'));
-				$masonry_gallery.isotope('layout');
-			});
-		
-		});
-	}
+			    // changing layout
+			    jQuery('#grid-changer li a').on('click', function(){
+				    jQuery('#grid-changer li a').removeClass('active');
+				    jQuery(this).toggleClass('active');
+
+				    $masonry_items.removeClass('col-3');
+				    $masonry_items.removeClass('col-4');
+				    $masonry_items.removeClass('col-5');
+				    $masonry_items.toggleClass(jQuery(this).closest('li').attr('class'));
+				    $masonry_gallery.isotope('layout');
+			    });
+		    
+		    });
+	    }
+
+	    
+	    // before-after
+	    var $before_after = jQuery('.before-after.gallery');
+	    if ( $before_after.length > 0 ) {
+	      $before_after.imageReveal({
+			    barWidth: 4,
+			    touchBarWidth: 50,
+			    startPosition: 0.5,
+			    width: jQuery('.before-after img').width(),
+			    height:  jQuery('.before-after img').height()
+	      });
+	    }
     });
-}
-		
-function beforeafter($){
-    var $before_after = jQuery('.before-after.gallery');
-    if ( $before_after.length > 0 ) {
-	    $before_after.imageReveal({
-		    barWidth: 4,
-		    touchBarWidth: 50,
-		    startPosition: 0.5,
-		    width: jQuery('.before-after img').width(),
-		    height:  jQuery('.before-after img').height()
-	    });
-    }
+	
+
+} )( jQuery );
+
 }
