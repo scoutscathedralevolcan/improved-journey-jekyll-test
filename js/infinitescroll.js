05d6cca0dd5d5fa4e95732f61fb9aa6c64f49d07
Mathieu Simard <mathieu.simard.14@gmail.com>
2016-12-05 00:08:23 -0500

    New posts layout

--- js/infinitescroll.js
+++ js/infinitescroll.js
@@ -3,6 +3,7 @@ $(window).scroll(function() {
     // check if this is greater than the document height minus 300
     if ($(window).scrollTop() + $(window).height() >= $(document).height() - 300) {
         loadMoreContent();
+	masonry_gallery();
     }
 });
 
@@ -37,4 +38,70 @@ function loadMoreContent() {
     paginationnav.hide();
     
 }
+
+
+
+
+/* ********* WINDOW LOAD ********** */
+jQuery(window).load(function() {
+    masonry_gallery()
+    beforeafter()
+    var paginationnav = $("div.paginationnav").first();
+    paginationnav.hide();
+});	
+	
+function masonry_gallery()
+{
+    var $masonry_gallery = jQuery('.masonry-gallery.gallery');
+    if ( $masonry_gallery.length > 0 ) {
+
+	    $masonry_gallery.each( function(index, element) {
+		    var $masonry_items = $(element).find('.gallery-item');
+	    
+		    // set masonry layout
+		    $(element).isotope({
+			    masonry: { columnWidth: $(element).find('.gallery-item')[0] },
+			    itemSelector: '.gallery-item'
+		    });
+		    $(element).isotope('layout');
+			    
+		    // filtering
+		    jQuery('#gallery-filter li a').on('click', function(){
+			    jQuery('#gallery-filter li a').removeClass('active');
+			    jQuery(this).addClass('active');
+			    var selector = jQuery(this).attr('data-filter');
+			    $masonry_gallery.isotope({ filter: selector });
+			    return false;
+		    });
+
+		    // changing layout
+		    jQuery('#grid-changer li a').on('click', function(){
+			    jQuery('#grid-changer li a').removeClass('active');
+			    jQuery(this).toggleClass('active');
+
+			    $masonry_items.removeClass('col-3');
+			    $masonry_items.removeClass('col-4');
+			    $masonry_items.removeClass('col-5');
+			    $masonry_items.toggleClass(jQuery(this).closest('li').attr('class'));
+			    $masonry_gallery.isotope('layout');
+		    });
+	    
+	    });
+    }
+}
+		
+function beforeafter(){
+    var $before_after = jQuery('.before-after.gallery');
+    if ( $before_after.length > 0 ) {
+	    $before_after.imageReveal({
+		    barWidth: 4,
+		    touchBarWidth: 50,
+		    startPosition: 0.5,
+		    width: jQuery('.before-after img').width(),
+		    height:  jQuery('.before-after img').height()
+	    });
+    }
+}
+
+
 
