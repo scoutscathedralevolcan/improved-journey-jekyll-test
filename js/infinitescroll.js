ec7846a89d35f99de2f119552ae537e3fe79713d
Mathieu Simard <mathieu.simard.14@gmail.com>
2016-12-05 02:19:46 -0500

    Script modif

--- js/infinitescroll.js
+++ js/infinitescroll.js
@@ -2,8 +2,8 @@ $(window).scroll(function() {
     // get current scroll top in px, add window height and
     // check if this is greater than the document height minus 300
     if ($(window).scrollTop() + $(window).height() >= $(document).height() - 300) {
-        loadMoreContent(jQuery);
-	masonry_gallery(jQuery);
+        loadMoreContent();
+	$(window).load(masonry_gallery());
     }
 });
 
@@ -49,62 +49,54 @@ $(window).load(function() {
 	
 function masonry_gallery()
 {
-  
-  (function( $ ){
-    jQuery(window).load(function() {
-	    // masonry gallery
-	    var $masonry_gallery = jQuery('.masonry-gallery.gallery');
-	    if ( $masonry_gallery.length > 0 ) {
+  // masonry gallery
+  var $masonry_gallery = $('.gallery.masonry-gallery.post-list').first();
+  if ( $masonry_gallery.length > 0 ) {
+	  $masonry_gallery.each( function(index, element) {
+		  var $masonry_items = $(element).find('.gallery-item');
+	  
+		  // set masonry layout
+		  $(element).isotope({
+			  masonry: { columnWidth: $(element).find('.gallery-item')[0] },
+			  itemSelector: '.gallery-item'
+		  });
+		  $(element).isotope('layout');
+			  
+		  // filtering
+		  $('#gallery-filter li a').on('click', function(){
+			  $('#gallery-filter li a').removeClass('active');
+			  $(this).addClass('active');
+			  var selector = $(this).attr('data-filter');
+			  $masonry_gallery.isotope({ filter: selector });
+			  return false;
+		  });
 
-		    $masonry_gallery.each( function(index, element) {
-			    var $masonry_items = $(element).find('.gallery-item');
-		    
-			    // set masonry layout
-			    $(element).isotope({
-				    masonry: { columnWidth: $(element).find('.gallery-item')[0] },
-				    itemSelector: '.gallery-item'
-			    });
-			    $(element).isotope('layout');
-				    
-			    // filtering
-			    jQuery('#gallery-filter li a').on('click', function(){
-				    jQuery('#gallery-filter li a').removeClass('active');
-				    jQuery(this).addClass('active');
-				    var selector = jQuery(this).attr('data-filter');
-				    $masonry_gallery.isotope({ filter: selector });
-				    return false;
-			    });
+		  // changing layout
+		  $('#grid-changer li a').on('click', function(){
+			  $('#grid-changer li a').removeClass('active');
+			  $(this).toggleClass('active');
 
-			    // changing layout
-			    jQuery('#grid-changer li a').on('click', function(){
-				    jQuery('#grid-changer li a').removeClass('active');
-				    jQuery(this).toggleClass('active');
+			  $masonry_items.removeClass('col-3');
+			  $masonry_items.removeClass('col-4');
+			  $masonry_items.removeClass('col-5');
+			  $masonry_items.toggleClass($(this).closest('li').attr('class'));
+			  $masonry_gallery.isotope('layout');
+		  });
+	  
+	  });
+  }
 
-				    $masonry_items.removeClass('col-3');
-				    $masonry_items.removeClass('col-4');
-				    $masonry_items.removeClass('col-5');
-				    $masonry_items.toggleClass(jQuery(this).closest('li').attr('class'));
-				    $masonry_gallery.isotope('layout');
-			    });
-		    
-		    });
-	    }
 
-	    
-	    // before-after
-	    var $before_after = jQuery('.before-after.gallery');
-	    if ( $before_after.length > 0 ) {
-	      $before_after.imageReveal({
-			    barWidth: 4,
-			    touchBarWidth: 50,
-			    startPosition: 0.5,
-			    width: jQuery('.before-after img').width(),
-			    height:  jQuery('.before-after img').height()
-	      });
-	    }
+  // before-after
+  var $before_after = $('.before-after.gallery');
+  if ( $before_after.length > 0 ) {
+    $before_after.imageReveal({
+		  barWidth: 4,
+		  touchBarWidth: 50,
+		  startPosition: 0.5,
+		  width: $('.before-after img').width(),
+		  height:  $('.before-after img').height()
     });
-	
-
-} )( jQuery );
+  }
 
 }
