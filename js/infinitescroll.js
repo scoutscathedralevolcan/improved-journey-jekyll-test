0fc0b42954034d654bde50b29acd6c3636292d0e
Mathieu Simard <mathieu.simard.14@gmail.com>
2016-12-05 01:05:31 -0500

    Testing

--- js/infinitescroll.js
+++ js/infinitescroll.js
@@ -32,7 +32,7 @@ function loadMoreContent() {
                 );
             }
         });
-	paginationnav.load(url + " div.paginationnav")
+	paginationnav.load(url + " div.paginationnav");
     });
     
 }
@@ -50,7 +50,7 @@ $(window).load(function() {
 	
 function masonry_gallery()
 {
-    var $masonry_gallery = $("div.gallery.masonry-gallery.gallery.post-list");
+    var $masonry_gallery = $("div.gallery.masonry-gallery.post-list");
     var next = $("a.next").first();
     next.each(function(key, value) {
       var url = $(value).attr('href');
