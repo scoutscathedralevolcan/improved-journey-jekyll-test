5837fe721383ce5e524f180bbf31c09a3bd53e7a
Mathieu Simard <mathieu.simard.14@gmail.com>
2016-12-06 02:15:08 -0500

    fix

--- js/infinitescroll.js
+++ js/infinitescroll.js
@@ -53,11 +53,8 @@ function masonry_actu()
       var $masonry_actu = $('div.post-list').first();
       if ( $masonry_actu.length > 0 ) {
 	$masonry_actu.isotope({
-	  layoutMode: 'fitRows',
+	  layoutMode: 'fitColumns',
 	  percentPosition: true,
-	  fitRows: {
-	    gutter: '.gutter-sizer'
-	  },
 	  itemSelector: '.post'
 	});
       }
