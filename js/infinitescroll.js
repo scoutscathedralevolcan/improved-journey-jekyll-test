9a18328f1aa6950782efe32e9041aa3269d4bc68
Mathieu Simard <mathieu.simard.14@gmail.com>
2016-12-06 02:12:39 -0500

    fix

--- js/infinitescroll.js
+++ js/infinitescroll.js
@@ -53,7 +53,7 @@ function masonry_actu()
       var $masonry_actu = $('div.post-list').first();
       if ( $masonry_actu.length > 0 ) {
 	$masonry_actu.isotope({
-	  layoutMode: 'fitRows'
+	  layoutMode: 'fitRows',
 	  percentPosition: true,
 	  fitRows: {
 	    gutter: '.gutter-sizer'
