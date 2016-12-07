ff59e01f3ae3c7cca4b3c699eb72d8fab75ad440
Mathieu Simard <mathieu.simard.14@gmail.com>
2016-12-07 03:01:17 -0500

    knburns-gallery as home header

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 (function( $ ){
 
 	// fullscreen gallery
-	var $fullscreen_gallery = jQuery('.fullscreen-gallery.gallery');
+	var $fullscreen_gallery = $( "#fullscreen-gallery" );
 	if ( $fullscreen_gallery.length > 0 ) {
 
 		if ( $fullscreen_gallery.find('.gallery-item').length > 1 ) { // if there are more than 1 image
@@ -79,30 +79,6 @@
 			}
 		}
 	}
-
-
-
-	// kenburns on one featured image header image
-	var $kenburns = jQuery('.kenburns-gallery.gallery');
-	if ( $kenburns.length > 0 ) {
-		var gallery_set = [];
-		$kenburns.find('.gallery-icon img').each( function() {
-			gallery_set.push(jQuery(this).attr('src'));
-		});
-
-		jQuery('#kenburns').attr('width', jQuery(window).width());
-		jQuery('#kenburns').attr('height', jQuery(window).height());
-		jQuery('#kenburns').kenburns({
-			images: gallery_set,
-			frames_per_second: 30,
-			display_time: 5000,
-			fade_time: 1000,
-			zoom: 1.2,
-			background_color:'#F7F6F5'
-		});
-	}
-
-	
 	
 	
 	/* ********* WINDOW LOAD ********** */
